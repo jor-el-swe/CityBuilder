b862a1d2a48ce656553344899fd10c6930efa120
Joel Ring <joel.ring@gmail.com>
2021-02-03 18:07:15 +0100

    collider test

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wall : MonoBehaviour{
+
+    public GameObject IsometricGrid;
     
     private Color originalColor;
     private Color colorIncrease = new Color(1.3f, 1.3f, 1.3f); 
-    private Color colorDecrease = new Color(0.5f, 0.5f, 0.5f); 
+    private Color colorDecrease = new Color(0.5f, 0.5f, 0.5f);
     
     
     
-    //private bool buttonHoldDown;
     private bool wallSelected;
     private bool mouseButtonHoldDown;
 
     private Vector3 screenPoint;
     private Vector3 offset;
+    public List<Collider2D> colliders2D = new List<Collider2D>();
 
-    [SerializeField]private List<Transform> collidingTransforms;
     
     
     // Start is called before the first frame update
     void Start(){
         originalColor = this.GetComponentInChildren<SpriteRenderer>().color;
-        collidingTransforms = new List<Transform>();
+        var colliders = IsometricGrid.GetComponentsInChildren<Collider2D>();
+
+        foreach (var cell in colliders){
+            colliders2D.Add(cell);
+        }
     }
     
     private void OnMouseEnter(){
@@ -45,6 +50,12 @@ public class Wall : MonoBehaviour{
 
     private void OnMouseUp(){
         mouseButtonHoldDown = false;
+        
+        foreach (var cell in colliders2D){
+            if (this.GetComponent<Collider2D>().bounds.Intersects(cell.bounds)){
+                Debug.Log("touched cell!");
+            }
+        }
     }
 
     private void OnMouseExit(){
@@ -57,17 +68,7 @@ public class Wall : MonoBehaviour{
     private void OnMouseDrag(){
         var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        Debug.Log("current position: " + curPosition);
+        //Debug.Log("current position: " + curPosition);
         transform.position = curPosition;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other){
-        Debug.Log("entering colliding");
-        collidingTransforms.Add(other.transform);
-    }
-
-    private void OnCollisionExit2D(Collision2D other){
-        Debug.Log("exiting colliding");
-        collidingTransforms.Remove(other.transform);
     }
 }
